82cd7d121a4ca93322171d8714a8212224eb4461
metasharp <1176758572@qq.com>
2026-05-25 11:12:52 +0800

    将数据库操作从model目录迁移至dao目录

--- src/service/system/sys_dept_service.rs
+++ src/service/system/sys_dept_service.rs
@@ -4,7 +4,6 @@ use crate::dao::system::sys_dept_dao;
 use crate::dao::system::sys_dept_dao::SysDeptDao;
 use crate::model::system::sys_dept_model::Dept;
 use crate::vo::system::sys_dept_vo::{DeleteDeptReq, DeptReq, DeptResp, QueryDeptDetailReq, QueryDeptListReq, UpdateDeptStatusReq};
-use axum::Json;
 use rbatis::rbatis_codegen::ops::AsProxy;
 use rbatis::RBatis;
 use rbs::value;
